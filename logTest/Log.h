e569612e8c2dfd21cbeb520c394872ef267ede8f
ProgrammerHawk <ani.chipmunk@gmail.com>
2021-01-25 12:41:58 -0800

    I added comments for notes

--- logTest/Log.h
+++ logTest/Log.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 
+/*notes: */
+
 #define errorTryStart try{
 
 #define errorEndTry }
@@ -10,16 +12,20 @@
 #define errorCatchStart catch(int type){
 
 #define errorEndCatch }
+//________________________________________________
+
 
 template <class T>
 void log(T printable)
 {
+    /*notes: */
     std::cout << printable << std::endl;
 }
 
 template <class L>
 void logln (L printable)
 {
+    /*notes: */
     std::cout << printable;
 }
 
