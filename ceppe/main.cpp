698676018676b1c508c9ced80c32cb240b594d26
Ani Iyengar <ani.chipmunk@gmail.com>
2021-01-25 09:29:32 -0800

    Initial Commit

---
+++ ceppe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ceppe
+//
+//  Created by sowmya on 1/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
