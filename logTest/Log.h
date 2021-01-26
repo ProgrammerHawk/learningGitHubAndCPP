547b852e328fcc2fe5d59bf9293db792bb7c3391
ProgrammerHawk <ani.chipmunk@gmail.com>
2021-01-25 17:10:26 -0800

    I made it more readable

--- logTest/Log.h
+++ logTest/Log.h
@@ -5,13 +5,13 @@
 
 /*notes: */
 
-#define errorTryStart try{
+#define TStart try{
 
-#define errorEndTry }
+#define ETry }
 
-#define errorCatchStart catch
+#define CStart catch
 
-#define errorEndCatch }
+#define ECatch }
 //________________________________________________
 
 
@@ -36,7 +36,11 @@ public:
     std::string level;
     LoggingLevel(std::string Lvl)
     {
-        if (Lvl != "Working" && Lvl != "Not Working" && Lvl != "Runtime Error" && Lvl != "Compiling Error" && Lvl != "NULL")
+        if (Lvl != "Working"
+            && Lvl != "Not Working"
+            && Lvl != "Runtime Error"
+            && Lvl != "Compiling Error"
+            && Lvl != "NULL")
         {
             log("Invalid level");
         }
@@ -49,7 +53,11 @@ public:
     
     void switchLevel(std::string lvl)
     {
-        if (lvl != "Working" && lvl != "Not Working" && lvl != "Runtime Error" && lvl != "Compiling Error" && lvl != "NULL")
+        if (lvl != "Working"
+            && lvl != "Not Working"
+            && lvl != "Runtime Error"
+            && lvl != "Compiling Error"
+            && lvl != "NULL")
         {
             log("Invalid level");
         }
